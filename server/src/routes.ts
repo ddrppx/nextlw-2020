4eeff26f5bb74c4a0699d7d1aece749d25965c00
ddrppx <ddrppx@gmail.com>
2020-06-04 14:30:38 -0300

    deploy transaction

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,8 +27,10 @@ routes.post('/points', async (request, response) => {
         uf,
         items
     } = request.body;
+        //Transaction ("trx", substitues knex)
+    const trx = await knex.transaction();
 
-    await knex('points').insert({
+    const insertedIds = await trx('points').insert({
         image: 'image-fake',
         name,
         email,
@@ -38,6 +40,17 @@ routes.post('/points', async (request, response) => {
         city,
         uf
     });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        }
+    })
+
+    await trx('point_items').insert(pointItems);
 
     return response.json({ success: true });
 });
