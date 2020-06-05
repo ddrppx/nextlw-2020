3b3912ca72152feafbcf602680b7fd8840d86e8e
ddrppx <ddrppx@gmail.com>
2020-06-05 11:36:46 -0300

    lading page, begining routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
 
 function App() {
-return(
-      <Home />
+  return(
+      <CreatePoint />
   );
-}
+};
 
 export default App;
