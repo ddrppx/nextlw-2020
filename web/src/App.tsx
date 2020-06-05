1723d63c3cb60234bfb955d999a6fa1611b876d5
ddrppx <ddrppx@gmail.com>
2020-06-05 10:18:04 -0300

    Exploring react js

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Header from './Header';
+
 function App() {
-  return (
-        <h1>Hello Worlds</h1>
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick(){
+    setCounter(counter + 1);
+  }
+
+  return(
+    <div>
+      <Header title="Hello Worlds"/> 
+
+      <h1>{counter}</h1>
+      <h1>{counter* 2}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
